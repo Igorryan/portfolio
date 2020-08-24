eca9de66e2c536b5a45e875d9083c6c35c99e739
Igor Ryan <dev@igorryan.com>
2020-08-24 14:00:01 -0300

    description brodti.ti

--- src/pages/Brodti/index.tsx
+++ src/pages/Brodti/index.tsx
@@ -46,22 +46,21 @@ const Brodti: React.FC = () => {
     <Container>
       <Sidebar title='BrodTi'
         hardSkills={hardSkills} logo={Logo}>
-        I am a software engineering student and currently do internship at <a href="https://www.igti.com.br/" rel="noopener noreferrer" target="_blank">
-          Institute of the technology</a> and work as a freelance fullstack developer mainly in <a href="https://pt-br.reactjs.org/" rel="noopener noreferrer" target="_blank">React.js</a>
-      and <a href="https://reactnative.dev/" rel="noopener noreferrer" target="_blank">React Native</a>. I'm a CSS nerd and I also venture into the user interface design.
+        In this freelancer project, I received the layout of the entire site to develop. I implemented it faithfully and took the opportunity to add an addictive slide show.
+        <br /><a href="https://www.brodti.com/" rel="noopener noreferrer" target="_blank">www.brodti.com</a>
       </Sidebar>
-      <Section>
-        <Video autoPlay loop muted playsInline>
-          <source src={video1} />
-        </Video>
-        <Video autoPlay loop muted playsInline>
-          <source src={video2} />
-        </Video>
-        <Video autoPlay loop muted playsInline>
-          <source src={video3} />
-        </Video>
-        <Image width="100%" src={mobile} alt="mobile screens" />
-      </Section>
+          <Section>
+            <Video autoPlay loop muted playsInline>
+              <source src={video1} />
+            </Video>
+            <Video autoPlay loop muted playsInline>
+              <source src={video2} />
+            </Video>
+            <Video autoPlay loop muted playsInline>
+              <source src={video3} />
+            </Video>
+            <Image width="100%" src={mobile} alt="mobile screens" />
+          </Section>
     </Container>
   );
 }
