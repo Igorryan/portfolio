4bcfcb2078f30ecdb3e81064ea73af3f9a3ebf15
Igor Ryan <igorryan10@gmail.com>
2022-11-07 20:42:30 -0300

    all projects

--- src/data/Projects.ts
+++ src/data/Projects.ts
@@ -7,6 +7,9 @@ import PicPayMovie from '../assets/videos/picpay.mp4';
 import TwitterMovie from '../assets/videos/twitter.mp4';
 import GoBarberMovie from '../assets/videos/gobarber.mp4';
 import SpiderManMovie from '../assets/videos/spiderman.mp4';
+import CozinhaFitnessMovie from '../assets/videos/cozinhafitness.mp4';
+import ExclusiveMovie from '../assets/videos/exclusive.mp4';
+import LaTorreMovie from '../assets/videos/latorre.mp4';
 
 interface ILinkProps {
   url: string;
@@ -86,7 +89,7 @@ const Projects: IProject[] = [
       },
     ],
   },
-    {
+  {
     title: 'MyTipLife',
     uptitle: 'Freelancer',
     local: 'San Francisco ─ California',
@@ -106,7 +109,7 @@ const Projects: IProject[] = [
       },
     ],
   },
-    {
+  {
     title: 'PUC Minas',
     uptitle: 'First Deploy',
     local: 'Belo Horizonte ─ Brazil',
@@ -123,7 +126,7 @@ const Projects: IProject[] = [
       },
     ],
   },
-    {
+  {
     title: 'PicPay Clone',
     uptitle: 'Study Project',
     local: 'Sete Lagoas - Brazil',
@@ -156,7 +159,7 @@ const Projects: IProject[] = [
     ],
   },
 
-    {
+  {
     title: 'GoBarber',
     uptitle: 'Rocketseat',
     local: 'Belo Horizonte - Brazil',
@@ -193,168 +196,55 @@ const Projects: IProject[] = [
       },
     ],
   },
-    {
-    title: 'API Net Promoter Score (NPS)',
-    uptitle: 'XPE',
-    local: 'Belo Horizonte ─ Brazil',
-    date: new Date(2020, 3, 14),
+  {
+    title: 'Cozinha Fitness',
+    uptitle: 'Freelancer',
+    local: 'Sete Lagoas - Brazil',
+    preview: CozinhaFitnessMovie,
+    date: new Date(2021, 6, 28),
     tags: [
-      'Node.js',
-      'Docker',
+      'UI/UX'
     ],
     links: [
       {
-        name: 'GitHub',
-        url: 'https://github.com/Igorryan/nps-igti'
+        name: 'Figma',
+        url: 'https://www.figma.com/file/jT2yHYbTQHCm90fUA5eCAg/Cozinha-Fitness?node-id=146%3A0'
+      }
+    ],
+  },
+  {
+    title: 'Exclusive E-commerce',
+    uptitle: 'Freelancer',
+    local: 'Sete Lagoas - Brazil',
+    preview: ExclusiveMovie,
+    date: new Date(2020, 4, 6),
+    tags: [
+      'UX/UX',
+    ],
+    links: [
+      {
+        name: 'Figma',
+        url: 'https://www.figma.com/file/NRbVlPTzc30AMF4hFzHQ2L/Exclusive-E-Commerce?node-id=110%3A1284'
       },
     ],
   },
-  // {
-  //   title: 'Cozinha Fitness',
-  //   uptitle: 'Freelancer',
-  //   local: 'Sete Lagoas - Brazil',
-  //   date: new Date(2021, 6, 28),
-  //   tags: [
-  //     'UI/UX'
-  //   ],
-  //   links: [
-  //     {
-  //       name: 'Figma',
-  //       url: 'https://www.figma.com/file/SEB18kfCRyrWFzrZ1ZqEVG/Cozinha-Fitness?node-id=0%3A1'
-  //     }
-  //   ],
-  // },
-  // {
-  //   title: 'Arena Trade',
-  //   uptitle: 'Nick Builder',
-  //   local: 'Belo Horizonte - Brazil',
-  //   date: new Date(2021, 6, 22),
-  //   tags: [
-  //     'UI/UX',
-  //     'Angular'
-  //   ],
-  //   links: [
-  //     {
-  //       name: 'Figma',
-  //       url: 'https://www.figma.com/file/oQIT9vq54TBD35zUjwSVl2/Arena-Trade?node-id=0%3A1'
-  //     }
-  //   ],
-  // },
-
-  // {
-  //   title: 'GiftCards Restaurants',
-  //   uptitle: 'Freelancer',
-  //   local: 'Sete Lagoas - Brazil',
-  //   date: new Date(2020, 12, 18),
-  //   tags: [
-  //     'PHP',
-  //     'JQuery'
-  //   ],
-  //   links: [
-  //     {
-  //       name: 'Website',
-  //       url: 'https://www.mytiplife.com/marketplace'
-  //     }
-  //   ],
-  // },
-  // {
-  //   title: 'BrodTi',
-  //   uptitle: 'Freelancer',
-  //   local: 'San Francisco ─ California',
-  //   date: new Date(2020, 7, 9),
-  //   tags: [
-  //     'HTML',
-  //     'CSS',
-  //     'JavaScript',
-  //   ],
-  //   links: [
-  //     {
-  //       name: 'GitHub',
-  //       url: 'https://github.com/Igorryan/brodti'
-  //     },
-  //   ],
-  // },
-  // {
-  //   title: 'API Net Promoter Score (NPS)',
-  //   uptitle: 'IGTI',
-  //   local: 'Belo Horizonte ─ Brazil',
-  //   date: new Date(2020, 3, 14),
-  //   tags: [
-  //     'Node.js',
-  //     'Docker',
-  //   ],
-  //   links: [
-  //     {
-  //       name: 'GitHub',
-  //       url: 'https://github.com/Igorryan/nps-igti'
-  //     },
-  //   ],
-  // },
-
-
-  // {
-  //   title: 'GoStack',
-  //   uptitle: 'Study Projects',
-  //   local: 'Belo Horizonte - Brazil',
-  //   date: new Date(2020, 6, 9),
-  //   tags: [
-  //     'ReactJS | Native',
-  //     'Node.js',
-  //   ],
-  //   links: [
-  //     {
-  //       name: 'GitHub',
-  //       url: 'https://github.com/Igorryan/rocketseat-gostack'
-  //     },
-  //   ],
-  // },
-  // {
-  //   title: 'GoBarber',
-  //   uptitle: 'Rocketseat',
-  //   local: 'Belo Horizonte - Brazil',
-  //   date: new Date(2020, 6, 8),
-  //   tags: [
-  //     'ReactJS | Native',
-  //     'Node.js',
-  //   ],
-  //   links: [
-  //     {
-  //       name: 'GitHub',
-  //       url: 'https://github.com/Igorryan/gobarber'
-  //     },
-  //   ],
-  // },
-  // {
-  //   title: 'Clones',
-  //   uptitle: 'Study Project',
-  //   local: 'Sete Lagoas - Brazil',
-  //   date: new Date(2020, 6, 13),
-  //   tags: [
-  //     'Sass',
-  //     'React Native',
-  //   ],
-  //   links: [
-  //     {
-  //       name: 'GitHub',
-  //       url: 'https://github.com/Igorryan/apps-clones'
-  //     },
-  //   ],
-  // },
-  // {
-  //   title: 'Exclusive E-commerce',
-  //   uptitle: 'Freelancer',
-  //   local: 'Sete Lagoas - Brazil',
-  //   date: new Date(2020, 4, 6),
-  //   tags: [
-  //     'UX/UX',
-  //   ],
-  //   links: [
-  //     {
-  //       name: 'Figma',
-  //       url: 'https://www.figma.com/file/0vaggidrXGW9T5bvgfmuq5/Exclusive-Web-Plataform?node-id=0%3A1'
-  //     },
-  //   ],
-  // },
+  {
+    title: 'Y-Hotel',
+    uptitle: 'Resort La Torre',
+    local: 'Bahia - Brazil',
+    preview: LaTorreMovie,
+    date: new Date(2022, 2, 1),
+    tags: [
+      'UX/UX',
+      'Next.js',
+    ],
+    links: [
+      {
+        name: 'Figma',
+        url: 'https://www.figma.com/file/NRbVlPTzc30AMF4hFzHQ2L/Exclusive-E-Commerce?node-id=110%3A1284'
+      },
+    ],
+  },
 ]
 
 export default Projects
